2b7f3f24e6be1e07b21816df42736b0ea957a419
Büşra Altuntaş <99415208+busraaltuuntas@users.noreply.github.com>
2023-03-22 00:07:04 +0300

    odev1

---
+++ BootcampOdev1/app/src/main/java/com/example/bootcampodev1/Odev1/Odev1.kt
@@ -0,0 +1,97 @@
+package com.example.bootcampodev1.Odev1
+
+fun main() {
+    var sehir : String= "İstanbul"
+    var ulke :String = "İtalya"
+    var telefon : Int = 5564433
+    var postaKodu : Int= 27000
+    var email : String = "busraaltuuntas "
+    var meslek : String = "Mühendis"
+    var stokMiktari : Int = 10
+    var musteriAdi : String= "Büşra"
+    var bakiye : Int =5432
+    var dogumGunu : String = "07-07-1997"
+    var maas : Int = 18000
+    var medeniDurum : String = "Bekar"
+    var urunYorum : String = "güzel"
+    var odemeTarihi : String = "01-02-2023"
+    var odeme : Double = 67.32
+    var siparisAdeti : Int = 8
+    var arabaModeli: String = "BMW"
+    var kitapAdi : String = "Bugün kalan hayatımın ilk günü"
+    var yayinlanmaTarihi : String = "2012"
+    var indirimMiktari : Int = 43
+    var odaSayisi : Int = 19
+    var enlem : String = "36-42 kuzey"
+    var boylam : String = "26-45 doğu"
+    var urunAdi : String = "bilgisayar"
+    var yemekFiyati : Double = 92.46
+    var marka : String = "ASUS"
+    var muzikAdi : String = "Unstoppable"
+    var videoSuresi : Double = 7.42
+    var urunPuani : Double = 3.9
+    var resimAdi : String = "tatil"
+    var dosyaFormati : String = "PDF"
+    var renk : String = "mavi"
+    var renkKodu : String = "#FFEBEE"
+    var telefonModeli : String = "Samsung Galaxy S20 FE"
+    var ekranBoyutu : Double = 5.8
+    var agirlik : Int = 72
+    var ulusalGun : String = "23 NİSAN ÇOCUK BAYRAMI"
+    var tatilGunu : String = "cumartesi"
+    var rezervasyonTarihi : String = "07-07-2023"
+    var sokakAdi : String = "İncilipınar"
+    var otobusHatti : String = "taksim-ortaköy"
+    var kalanDakika : Int = 17
+    var takipKodu : Int = 1234567
+    var kuponSuresi : String = "1 hafta"
+    var kuponKodu : String = "A7954323B"
+    var faturaAdresi : String = "nur apt kat 2"
+
+    println("sehir adi: $sehir")
+    println("ülke adi: $ulke")
+    println("telefon numarası: $telefon")
+    println("posta kodu: $postaKodu")
+    println("email: $email")
+    println("meslek: $meslek")
+    println("stok miktarı: $stokMiktari")
+    println("müşteri adı: $musteriAdi")
+    println("bakiye: $bakiye")
+    println("doğum günü: $dogumGunu")
+    println("maaş: $maas")
+    println("medeni durum: $medeniDurum")
+    println("ürün yorumu: $urunYorum")
+    println("ödeme tarihi: $odemeTarihi")
+    println("ödeme: $odeme")
+    println("sipariş adeti: $siparisAdeti")
+    println("araba modeli: $arabaModeli")
+    println("kitap adı: $kitapAdi")
+    println("yayınlanma tarihi: $yayinlanmaTarihi")
+    println("indirim miktarı: $indirimMiktari")
+    println("oda sayısı: $odaSayisi")
+    println("enlem: $enlem")
+    println("boylam: $boylam")
+    println("ürün adı: $urunAdi")
+    println("yemek fiyatı: $yemekFiyati")
+    println("marka: $marka")
+    println("müzik adı: $muzikAdi")
+    println("video süresi: $videoSuresi")
+    println("ürün puanı: $urunPuani")
+    println("resim adı: $resimAdi")
+    println("dosya formatı: $dosyaFormati")
+    println("renk: $renk")
+    println("renk kodu: $renkKodu")
+    println("telefon modeli: $telefonModeli")
+    println("ekran boyutu: $ekranBoyutu")
+    println("ağırlık: $agirlik")
+    println("ulusal gün: $ulusalGun")
+    println("tatil günü: $tatilGunu")
+    println("rezervasyon tarihi: $rezervasyonTarihi")
+    println("sokak adı: $sokakAdi")
+    println("otobüs hattı: $otobusHatti")
+    println("kalan dakika: $kalanDakika")
+    println("takip kodu: $takipKodu")
+    println("kupon süresi: $kuponSuresi")
+    println("kupon kodu: $kuponKodu")
+    println("fatura adresi: $faturaAdresi")
+}
